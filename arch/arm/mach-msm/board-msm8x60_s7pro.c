ef9fa6588d0801266289e8b42c4fac1601896a99
ubu <ubu@ubu-VirtualBox.(none)>
2013-09-19 22:40:28 +0200

    sync video: msm: hdmi with CAF M8260AAABQOCZM34730070

--- arch/arm/mach-msm/board-msm8x60_s7pro.c
+++ arch/arm/mach-msm/board-msm8x60_s7pro.c
@@ -3989,13 +3989,17 @@ static struct resource hdmi_msm_resources[] = {
 
 static int hdmi_enable_5v(int on);
 static int hdmi_core_power(int on, int show);
+static int hdmi_gpio_config(int on);
 static int hdmi_cec_power(int on);
+static int hdmi_panel_power(int on);
 
 static struct msm_hdmi_platform_data hdmi_msm_data = {
 	.irq = HDMI_IRQ,
 	.enable_5v = hdmi_enable_5v,
 	.core_power = hdmi_core_power,
 	.cec_power = hdmi_cec_power,
+        .panel_power = hdmi_panel_power,
+        .gpio_config = hdmi_gpio_config,
 };
 
 static struct platform_device hdmi_msm_device = {
@@ -11218,7 +11222,30 @@ static int hdmi_core_power(int on, int show)
 				"8058_l16", rc);
 			return rc;
 		}
-		rc = gpio_request(170, "HDMI_DDC_CLK");
+           pr_debug("%s(on): success\n", __func__);
+             } else {
+    rc = regulator_disable(reg_8058_l16);
+    if (rc)
+      pr_warning("'%s' regulator disable failed, rc=%d\n",
+        "8058_l16", rc);
+    pr_debug("%s(off): success\n", __func__);
+  }
+
+  prev_on = on;
+
+  return 0;
+}
+
+static int hdmi_gpio_config(int on)
+{
+         int rc = 0;
+         static int prev_on;
+
+         if (on == prev_on)
+         return 0;
+
+             if (on) {
+                rc = gpio_request(170, "HDMI_DDC_CLK");
 		if (rc) {
 			pr_err("'%s'(%d) gpio_request failed, rc=%d\n",
 				"HDMI_DDC_CLK", 170, rc);
@@ -11236,16 +11263,12 @@ static int hdmi_core_power(int on, int show)
 				"HDMI_HPD", 172, rc);
 			goto error3;
 		}
-		pr_info("%s(on): success\n", __func__);
+		pr_debug("%s(on): success\n", __func__);
 	} else {
 		gpio_free(170);
 		gpio_free(171);
 		gpio_free(172);
-		rc = regulator_disable(reg_8058_l16);
-		if (rc)
-			pr_warning("'%s' regulator disable failed, rc=%d\n",
-				"8058_l16", rc);
-		pr_info("%s(off): success\n", __func__);
+		pr_debug("%s(off): success\n", __func__);
 	}
 
 	prev_on = on;
@@ -11257,7 +11280,6 @@ error3:
 error2:
 	gpio_free(170);
 error1:
-	regulator_disable(reg_8058_l16);
 	return rc;
 }
 
@@ -11274,11 +11296,8 @@ static int hdmi_cec_power(int on)
 		_GET_REGULATOR(reg_8901_l3, "8901_l3");
 
 	if (on) {
-#if 0
-		rc = regulator_set_voltage(reg_8901_l3, 3000000, 3000000);
-#else
+
 		rc = regulator_set_voltage(reg_8901_l3, 3300000, 3300000);
-#endif
 		if (!rc)
 			rc = regulator_enable(reg_8901_l3);
 		if (rc) {
@@ -11308,6 +11327,19 @@ static int hdmi_cec_power(int on)
 error:
 	regulator_disable(reg_8901_l3);
 	return rc;
+}
+
+static int hdmi_panel_power(int on)
+{
+  int rc;
+
+  pr_debug("%s: HDMI Core: %s\n", __func__, (on ? "ON" : "OFF"));
+  rc = hdmi_core_power(on, 1);
+  if (rc)
+    rc = hdmi_cec_power(on);
+
+  pr_debug("%s: HDMI Core: %s Success\n", __func__, (on ? "ON" : "OFF"));
+  return rc;
 }
 
 #undef _GET_REGULATOR
@@ -11756,6 +11788,7 @@ static struct msm_bus_scale_pdata dtv_bus_scale_pdata = {
 
 static struct lcdc_platform_data dtv_pdata = {
 	.bus_scale_table = &dtv_bus_scale_pdata,
+        .lcdc_power_save = hdmi_panel_power,
 };
 
 static struct msm_bus_paths dtv_hdmi_prim_bus_scale_usecases[] = {
