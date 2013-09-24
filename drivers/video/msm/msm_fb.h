e4ea5ebcd7033423ab4c797478fc4b54056aa754
ubu <ubu@ubu-VirtualBox.(none)>
2013-09-24 21:24:44 +0200

    msm: display: non-blocking for overlay update
    Display commit will work as overlay commit when
    MDP_DISPLAY_COMMIT_OVERLAY flag is set. It will be
    non-blocking if wait_for_finish is false.
    
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    Change-Id: Ibd143dbfcd2fdd7a8fab3038c5af4e2fbc9ea198

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -210,8 +210,7 @@ struct msm_fb_data_type {
 };
 struct msm_fb_backup_type {
 	struct fb_info info;
-	struct fb_var_screeninfo var;
-	struct msm_fb_data_type mfd;
+	struct mdp_display_commit disp_commit;
  };
 
 
