f072193ef5c0e745993f14168371964aff03f663
ubu <ubu@ubu-VirtualBox.(none)>
2013-09-24 22:25:23 +0200

    video: msm: Remove old post processing overlay integration
    Remove old implementation of HSIC and QSEED smoothing/sharpening overlay
    integration. Need to replace with new implementation that better integrates
    the existing functions of the two APIs.
    
    CRs-Fixed: 380750
    Change-Id: Id3469fad16764ed88e74a8da75bb873f726ba366
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    Signed-off-by: Pravin Tamkhane <pravint@codeaurora.org>
    (cherry picked from commit 3118cb077358e472adb0db8e207a63bd84fd267f)

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -370,7 +370,6 @@ struct mdp4_overlay_pipe {
         uint64 bw_ab_quota;
 	uint64 bw_ib_quota;
 	uint32 luma_align_size;
-	struct mdp4_hsic_regs hsic_regs;
 	struct mdp_overlay req_data;
 	struct completion comp;
 	struct completion dmas_comp;
@@ -920,7 +919,6 @@ int mdp4_writeback_terminate(struct fb_info *info);
 uint32_t mdp_block2base(uint32_t block);
 int mdp_hist_lut_config(struct mdp_hist_lut_data *data);
 
-void mdp4_hsic_set(struct mdp4_overlay_pipe *pipe, struct dpp_ctrl *ctrl);
 void mdp4_hsic_update(struct mdp4_overlay_pipe *pipe);
 int mdp4_csc_config(struct mdp_csc_cfg_data *config);
 void mdp4_csc_write(struct mdp_csc_cfg *data, uint32_t base);
